bd84bb2870f5f76545eee6c7ce71ef28082a372f
Andres Merchan <yeissona.merchanl@utadeo.edu.co>
2017-11-15 21:25:15 -0500

    first commit

---
+++ CurrencyBitcoinConverter/CurrencyBitcoinConverter/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  CurrencyBitcoinConverter
+//
+//  Created by practica on 1/11/17.
+//  Copyright © 2017 practica. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    let currencyArray = ["ARS","AUD","BRL","CAD","CNY","COP","EUR","GBP","HKD","IDR","ILS","INR","JPY","LRD","MXN","NOK","NZD","PLN","RON","RUB"
+    ,"HUF","MZN","SVC"]
+    let simbolArray = ["$","$","R$","$","¥","$","€","£","$","Rp","₪","₹","¥","$","$","kr","$","zł","lei","₽"
+        ,"Ft","MT","$"]
+    
+    let baseURL="https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    var strPrice = ""
+    var finalURL: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        currencyPickerView.delegate=self
+        currencyPickerView.dataSource=self
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(currencyArray[row])
+        strPrice = simbolArray[row]
+        print(simbolArray[row])
+        finalURL=baseURL+currencyArray[row]
+        getBitcoinData(url: finalURL!)
+    }
+    
+    //MARK: - Networking
+    func getBitcoinData(url:String){
+        Alamofire.request(url, method: .get).responseJSON { (response) in
+            if response.result.isSuccess{
+                let bitcoinJSON: JSON = JSON(response.result.value!)
+                self.updateBitCoinData(json:bitcoinJSON)
+            }
+            else{
+                print ("Error: \(response.result.error)")
+                self.lblPrice.text="Se presento un problema"
+            }
+        }
+        
+    }
+    
+    func updateBitCoinData(json:JSON){
+        if let bitcoinResult=json["ask"].double {
+            
+            strPrice = strPrice+FormatNumber(number: Double(bitcoinResult))
+            lblPrice.text=strPrice
+        }
+        else{
+            lblPrice.text="Servicio no Disponible"
+        }
+        strPrice = ""
+    }
+    
+    func FormatNumber(number:Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        numberFormatter.groupingSeparator = "."
+        let returnValue = numberFormatter.string(from: NSNumber(value:number))
+        
+        return returnValue!
+    }
+}
+
---
+++ CurrencyBitcoinConverter/Pods/Target Support Files/Pods-CurrencyBitcoinConverter/Pods-CurrencyBitcoinConverter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CurrencyBitcoinConverter : NSObject
+@end
+@implementation PodsDummy_Pods_CurrencyBitcoinConverter
+@end
